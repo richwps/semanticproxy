526f1acbf30fbe803edb47f195f055a3fa09bde3
Felix Bensmann <felix.bensmann@hs-osnabrueck.de>
2014-09-04 14:21:47 +0200

    Fixed exchange host mechanism

--- SemanticProxy/src/main/java/de/hsos/richwps/sp/App.java
+++ SemanticProxy/src/main/java/de/hsos/richwps/sp/App.java
@@ -12,6 +12,7 @@ import de.hsos.richwps.sp.web.UpdateAccess;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.xmlbeans.XmlException;
 
@@ -28,30 +29,40 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Semantic Proxy is starting...");
+        System.out.println("SemanticProxy is starting...");
         
         //configure logging
         PropertyConfigurator.configure("log4j.properties");
-       
+        Logger.getLogger(App.class).info("SemanticProxy is starting...");
         
         //Load configuration
         Configuration config = new Configuration();
         File configFile = new File("config.xml");
         try {
             config.load(configFile);
-        } catch (IOException io) {
+        } catch (IOException e) {
+            Logger.getLogger(App.class).warn(e.getClass()+"-exception when loading config.xml: ",e);
             try {
+                Logger.getLogger(App.class).info("Writing default config.xml");
                 config.writeDefaultConfiguration(configFile);
-                config.load(configFile);
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-                e.printStackTrace(); //TODO: change to logging
+            } catch (IOException ioee) {  
+                Logger.getLogger(App.class).error("IO Exception when writing default config.xml: ",e);
+                Logger.getLogger(App.class).info("Shutdown due to error");
                 System.out.println("Shutdown due to error");
                 System.exit(-1);
             }
-        } catch (XmlException xml) {
-            System.err.println(xml.getMessage());
-            xml.printStackTrace();
+            try{
+                Logger.getLogger(App.class).info("Reading back default config.xml");
+                config.load(configFile);
+            }catch(XmlException | IOException io){
+                Logger.getLogger(App.class).error("IOException when reading back default config.xml",io);
+                Logger.getLogger(App.class).info("Shutdown due to error");
+                System.out.println("Shutdown due to error");
+                System.exit(-1);
+            }
+        } catch (XmlException e) {
+            Logger.getLogger(App.class).error("XMLException when reading config.xml. Is file malformatted?",e);
+            Logger.getLogger(App.class).info("Shutdown due to error");
             System.out.println("Shutdown due to error");
             System.exit(-1);
         }
@@ -64,8 +75,8 @@ public class App {
         try {
             Vocabulary.init(config.getVocabularyURL());
         } catch (Exception e) {
-            System.err.println(e.getMessage());
-            e.printStackTrace();
+            Logger.getLogger(App.class).error("Exception during initialization of vocabulary",e);
+            Logger.getLogger(App.class).info("Shutdown due to error");
             System.out.println("Shutdown due to error");
             System.exit(-1);
         }
@@ -84,6 +95,7 @@ public class App {
             //load initial data
             if (DBIO.size() == 0) {
                 //create the root resource if db was empty
+                Logger.getLogger(App.class).info("Inserting network node");
                 ContentChanger.insertNetwork(config.getOwner(), config.getDomain());
 
                 //insert rdf resources from predefined files
@@ -100,20 +112,30 @@ public class App {
                     //insert (modified) content into db
                     ContentChanger.insertWPS(content);
                     System.out.println("File " + list.get(i).getAbsolutePath() + " loaded");
+                    Logger.getLogger(App.class).info("File " + list.get(i).getAbsolutePath() + " loaded");
                 }
 
                 list = config.getProcessRDFFiles();
                 for (int i = 0; i < list.size(); i++) {
+                    //read rdf file
                     String content = TextFileReader.readPlainText(list.get(i));
+                    //replace wildcard string with host
+                    if (config.getReplaceableHost() != null) {
+                        if (content.contains(config.getReplaceableHost())) {
+                            content = content.replaceAll(config.getReplaceableHost(), config.getHostURL().toString());
+                        }
+                    }
+                    //insert (modified) content into db
                     ContentChanger.insertProcess(content);
                     System.out.println("File " + list.get(i).getAbsolutePath() + " loaded");
+                    Logger.getLogger(App.class).info("File " + list.get(i).getAbsolutePath() + " loaded");
                 }
             }
 
 
         } catch (Exception e) {
-            System.err.println(e.getMessage());
-            e.printStackTrace();
+            Logger.getLogger(App.class).error(e.getClass()+"+-exception during initialization of DB",e);
+            Logger.getLogger(App.class).info("Shutdown due to error");
             System.out.println("Shutdown due to error");
             System.exit(-1);
         }
@@ -136,5 +158,6 @@ public class App {
         SearchAccess.activate(config.getSearchURL());
 
         System.out.println("Semantic Proxy is listening");
+        Logger.getLogger(App.class).info("Semantic Proxy is listening");
     }
 }
